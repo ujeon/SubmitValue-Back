248b351836797f043aefc0466a2c69b128d3f8ad
ujeon_jang <wj.jang@newploy.net>
2021-07-27 09:45:39 +0900

    이전 화면으로 값을 직접 전달하는 방법

---
+++ SubmitValue-Back/FormViewController.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+class FormViewController: UIViewController {
+    @IBOutlet var email: UITextField!
+    @IBOutlet var isUpdate: UISwitch!
+    @IBOutlet var interval: UIStepper!
+    
+    @IBAction func onSubmit(_ sender: Any) {
+        let preVC = self.presentingViewController
+        guard let vc = preVC as? ViewController else {
+            return
+        }
+        
+        vc.paramEmail = self.email.text
+        vc.paramUpdate = self.isUpdate.isOn
+        vc.paramInterval = self.interval.value
+        
+        self.presentingViewController?.dismiss(animated: true)
+    }
+}
--- SubmitValue-Back/ViewController.swift
+++ SubmitValue-Back/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var resultEmail: UILabel!
+    @IBOutlet var resultUpdate: UILabel!
+    @IBOutlet var resultInterval: UILabel!
+    
+    var paramEmail: String?
+    var paramUpdate: Bool?
+    var paramInterval: Double?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    override func viewWillAppear(_ animated: Bool) {
+        print("viewWillAppear")
+        if let email = paramEmail {
+            resultEmail.text = email
+        }
+        
+        if let update = paramUpdate {
+            resultUpdate.text = update == true ? "자동갱신" : "자동갱신안함"
+        }
+        
+        if let interval = paramInterval {
+            resultInterval.text = "\(Int(interval)) 분마다"
+        }
+    }
 }
 
